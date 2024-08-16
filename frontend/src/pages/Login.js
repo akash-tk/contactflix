0f521588498d69b560d3da963bc1f6025fc90899
Akash T K <akash.tk333@gmail.com>
2024-08-16 16:56:55 +0530

    UI fixes in frontend

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,79 @@
+import React, { useContext, useState } from "react";
+import { NavLink } from "react-router-dom";
+
+import AuthContext from "../context/AuthContext";
+import ToastContext from "../context/ToastContext";
+
+function Login() {
+  const { toast } = useContext(ToastContext);
+  const { loginUser } = useContext(AuthContext);
+
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+
+    setCredentials({ ...credentials, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (!credentials.email || !credentials.password) {
+      toast.error("Please enter all the required fields");
+      return;
+    }
+
+    loginUser(credentials);
+  };
+
+  return (
+    <>
+      <h3>LOGIN</h3>
+
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="emailInput" className="form-label mt-4">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="emailInput"
+            aria-describedby="emailHelp"
+            name="email"
+            value={credentials.email}
+            onChange={handleInputChange}
+            placeholder="example@gmail.com"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="passwordInput" className="form-label mt-4">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="passwordInput"
+            name="password"
+            value={credentials.password}
+            onChange={handleInputChange}
+            placeholder="Enter Password"
+            required
+          />
+        </div>
+        <input type="submit" value="Login" className="btn btn-primary my-3" />
+        <p>
+          Don't have an account ?&nbsp;
+          <NavLink to="/register">Create One</NavLink>
+        </p>
+      </form>
+    </>
+  );
+}
+
+export default Login;
